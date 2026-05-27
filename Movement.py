c2cf4fd74adad66fb258d296754c83b4a116556c
Bobby Naughton <bnaughton@Bobbys-MacBook-Pro.local>
2026-05-27 14:58:07 -0400

    movement

--- Movement.py
+++ Movement.py
@@ -1,5 +1,22 @@
-def checkMoveValidity():
-    pass
+def checkMoveValidity(board, piece, potentialMove, potentialMoveOccupant, tracker, moveList):
+    #Checks move validity based on square of the potential move, valid moves will be added to move list
+    if(board.isWhite(piece)):
+        if(potentialMoveOccupant == 0):
+            moveList.append(potentialMove)
+        elif(potentialMoveOccupant < 0):
+            moveList.append(potentialMove)
+            tracker = False
+        else:
+            tracker = False
+    else:
+        if(potentialMoveOccupant == 0):
+            moveList.append(potentialMove)
+        elif(potentialMoveOccupant > 0 and potentialMoveOccupant != 99):
+            moveList.append(potentialMove)
+            tracker = False
+        else:
+            tracker = False
+    return tracker
 
 
 def bishopMovement():
